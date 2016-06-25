1897f6eed511b450199cc391934f08e107283225
chrismamo1 <christophermcalpine@gmail.com>
2016-06-25 14:52:35 -0500

    accidentally removed a critical line in my original commit

--- src/lib/uTop_main.cppo.ml
+++ src/lib/uTop_main.cppo.ml
@@ -182,6 +182,7 @@ class read_phrase ~term = object(self)
         try
           let result = parse_and_check input eos_is_error in
           return_value <- Some result;
+          LTerm_history.add UTop.history input;
           ignore(match result with
             | UTop.Value _, _ ->
                 LTerm_history.add UTop.history input;
