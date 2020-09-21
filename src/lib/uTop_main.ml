251228d677b26c51eb0d756f5da86458e6c85f6e
Haz <hyphens@pm.me>
2020-09-21 22:23:36 +0000

    Make sure we're not at $HOME before sourcing local .ocamlinit
    (fixes: Github #333)

--- src/lib/uTop_main.ml
+++ src/lib/uTop_main.ml
@@ -1479,7 +1479,7 @@ let common_init ~initial_env =
            None
          )
      | None ->
-         if Sys.file_exists ".ocamlinit" then
+         if Sys.file_exists ".ocamlinit" && Sys.getcwd () <> Sys.getenv "HOME" then
            Some ".ocamlinit"
          else
            let xdg_fn = LTerm_resources.xdgbd_file ~loc:LTerm_resources.Config "utop/init.ml" in
